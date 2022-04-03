6c2f254187faceb7361f73e8f44a858689c6174a
abinash <abinash55@gmail.com>
2022-04-03 16:48:36 +0530

    cmt msg

---
+++ src/test/java/org/gitcmd/cmd.java
@@ -0,0 +1,18 @@
+package org.gitcmd;
+
+public class cmd {
+
+	public static void main(String[] args) {
+		
+		
+		int a=1;
+		int b=2;
+		
+		int c=a+b;
+		
+		System.out.println(c);
+				
+
+	}
+
+}
